d2c84c27af420a1785e8d952493be447effe4b3d
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2023-06-25 15:28:06 +0600

    Progressing 1.1

--- HandlingWebElement.py
+++ HandlingWebElement.py
@@ -1,4 +1,5 @@
 from selenium  import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -6,5 +7,9 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options,service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("https://facebook.com")
+driver.get("https://discord.com/login")
 driver.maximize_window()
+
+driver.find_element(By.ID,'uid_5').send_keys("fardinaero1@gmail.com")
+driver.find_element(By.ID,'uid_7').send_keys("fardinaero1@gmail.com")
+driver.find_element(By.XPATH,'//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/button[2]').click()
