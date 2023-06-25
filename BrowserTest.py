de51edd082fdf6cd40aa6c050ebe5d3bdede716e
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2023-06-25 13:10:44 +0600

    Just Started

---
+++ BrowserTest.py
@@ -0,0 +1,17 @@
+from selenium  import webdriver
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(options=options,service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://facebook.com")
+driver.maximize_window()
+title = driver.title
+print(title)
+
+assert "Facebooks" in title
