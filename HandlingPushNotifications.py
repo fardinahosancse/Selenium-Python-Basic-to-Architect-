6f47ecc0100a0ded9d29f24dfed6ca2b851de5de
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2023-07-08 13:33:08 +0600

    Fix

--- HandlingPushNotifications.py
+++ HandlingPushNotifications.py
@@ -1,14 +1,15 @@
 import time
-from selenium  import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
 options = webdriver.ChromeOptions()
-perfs={"profile.default_content_setting_values.notification":2}
+prefs = {"profile.default_content_setting_values.notifications": 2}
 options.add_experimental_option("detach", True)
-options.add_experimental_option("perfs", perfs)
-driver = webdriver.Chrome(options=options,service=ChromeService(ChromeDriverManager().install()))
+options.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 driver.maximize_window()
 driver.implicitly_wait(10)
-
 
 driver.get("https://web.dev/push-notifications-display-a-notification/")
